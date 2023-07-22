eca21bd2ae049ad93d67d5f71a3b37dfb70e5f51
Eugene Pavlov <pavlov88ev@gmail.com>
2023-07-22 16:29:02 +0300

    23.props for Dialogs

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -1,10 +1,47 @@
 import React from "react";
 import s from './Dialogs.module.css'
+import {NavLink} from "react-router-dom";
+
+type DialogsItemPropsType = {
+    name: string
+    id: string
+}
+
+type MessagePropsType = {
+    message: string
+}
+
+const DialogsItem = (props: DialogsItemPropsType) => {
+    let path = "/dialogs/" + props.id
+    return (
+        <div className={s.dialog + ' ' + s.active}>
+            <NavLink to={path}>{props.name}</NavLink>
+        </div>
+    )
+}
+
+const Message = (props: MessagePropsType) => {
+    return (
+        <div className={s.message}>{props.message}</div>
+    )
+}
 
 const Dialogs = () => {
     return (
-        <div>
-            Dialogs
+        <div className={s.dialogs}>
+            <div className={s.dialogsItems}>
+                <DialogsItem name="Dimych" id="1"/>
+                <DialogsItem name="Andrey" id="2"/>
+                <DialogsItem name="Sveta" id="3"/>
+                <DialogsItem name="Sasha" id="4"/>
+                <DialogsItem name="Viktor" id="5"/>
+                <DialogsItem name="Valera" id="6"/>
+            </div>
+            <div className={s.messages}>
+                <Message message="Hi"/>
+                <Message message="How are you?"/>
+                <Message message="Yo"/>
+            </div>
         </div>
     )
 }
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,23 +1,24 @@
 import React from 'react';
 import s from './Navbar.module.css'
+import {NavLink} from "react-router-dom";
 
 const Navbar = () => {
     return (
         <nav className={s.nav}>
             <div className={s.item}>
-                <a>Profile</a>
+                <NavLink to='/profile' activeClassName={s.activeLink}>Profile</NavLink>
             </div>
             <div className={`${s.item} ${s.active}`}>
-                <a>Messages</a>
+                <NavLink to='/dialogs' activeClassName={s.activeLink}>Messages</NavLink>
             </div>
             <div className={s.item}>
-                <a>News</a>
+                <NavLink to='/news'>News</NavLink>
             </div>
             <div className={s.item}>
-                <a>Music</a>
+                <NavLink to='/music'>Music</NavLink>
             </div>
             <div className={s.item}>
-                <a>Settings</a>
+                <NavLink to='/settings'>Settings</NavLink>
             </div>
         </nav>
     );
